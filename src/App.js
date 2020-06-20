f0613bcd8e6b53b10e6a62839e7ebd09701cc6ee
Myat Thu Ko <nymyatthuko@gmail.com>
2020-06-20 19:06:39 -0400

    add changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import NavBar from './components/NarBar';
-import SignIn from './components/SignIn';
+import React, { Component } from "react";
+import NavBar from "./components/NarBar";
+import SignIn from "./components/SignIn";
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
