e842487a4f6ab288a18b7aecea685d37ae898a6d
XiaoleZ <xiaole2@ualberta.ca>
2020-06-20 13:32:12 -0700

    signup/signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './components/NarBar';
+import SignIn from './components/SignIn';
 // import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar />
+        <SignIn />
       </div>
     );
   }
