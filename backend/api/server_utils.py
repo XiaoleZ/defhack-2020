c3387ea68739198576d1ac1e76444ee141fbdbbc
ChrisChrisLoLo <sporewoh@gmail.com>
2020-06-20 21:01:53 -0600

    add login route

--- backend/api/server_utils.py
+++ backend/api/server_utils.py
@@ -1,5 +1,11 @@
 from flask import abort, jsonify, make_response
 
+def validate_required_fields(req_dict:dict, expected_keys:list)->None:
+    if not req_dict:
+        throw_error('No JSON')
+    for field in expected_keys:
+        if field not in req_dict:
+            throw_error(f'{field} was expected but not received')
 
-def throw_error(message):
+def throw_error(message:str)->None:
     abort(make_response(jsonify(message=message), 403))
