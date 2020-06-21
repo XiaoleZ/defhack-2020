f2bae3a14a7e992246a700721aa0849b96ecd684
Myat Thu Ko <nymyatthuko@gmail.com>
2020-06-20 21:39:46 -0400

    Part of Journal done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./components/NarBar";
 import SignIn from "./components/SignIn";
+import Journal from "./components/Journal";
 // import logo from './logo.svg';
 import "./App.css";
 
@@ -8,8 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <SignIn />
+        <Journal />
       </div>
     );
   }
--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,1 +1,28 @@
 // This is a journal session.
+import React, { Component } from "react";
+// import logo from './logo.svg';
+// import "./Journal.css";
+
+class Journal extends Component {
+  render() {
+    return (
+      <div class="Journal">
+        Let's write a journal! <br />
+        <label for="title">Title:</label> <br />
+        <input type="text" id="title" name="title" placeholder="Title" /> <br />
+        <label for="journalContent">Journal Content: </label> <br />
+        <textarea
+          id="journalContent"
+          name="journalContent"
+          rows="5"
+          cols="100"
+          placeholder="What're your thoughts? Just write it donw here!"
+        />
+        <br />
+        <button type="button">Save</button>
+      </div>
+    );
+  }
+}
+
+export default Journal;
