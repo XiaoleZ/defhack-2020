710d8ad9e6a0a676c66075be01a8bddc8ad32156
Myat Thu Ko <nymyatthuko@gmail.com>
2020-06-21 01:46:44 -0400

    Added Survey Questions

--- src/components/surveyData.js
+++ src/components/surveyData.js
@@ -1,25 +1,142 @@
 let surveyData = {
-  "questions":[
+  questions: [
     {
-      "id":"1",
-      "question":"Are you habituated to drugs and alcohol?",
-      "options": [`Yes`, `No`, `N/A`],
-      "answer": `Yes`,
+      id: "1",
+      question:
+        "Have you ever experienced a terrible occurrence that has impacted you significantly?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
     },
     {
-      "id":"2",
-      "question":"Is there a history of mental disorder in your family?",
-      "options": [`Yes`, `No`, `N/A`],
-      "answer": `Yes`,
+      id: "2",
+      question:
+        "Do you ever feel that you’ve been affected by feelings of edginess, anxiety, or nerves?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
     },
     {
-      "id":"3",
-      "question":"Have you ever been committed?",
-      "options": [`Yes`, `No`, `N/A`],
-      "answer": `Yes`,
-    }
+      id: "3",
+      question: "Have you ever had feelings of losing someone you care about?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "4",
+      question:
+        "Do you have any feelings of anxiety or discomfort around others that bother you?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "5",
+      question: "Have you ever had feelings of hurting someone or yourself?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "6",
+      question:
+        "Have you experienced any loss of your close family members, or your loved ones?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "7",
+      question:
+        "Are you currently on medication of any kind or in the past six months?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "8",
+      question:
+        "Have you experienced any suicidal thoughts in the past six months?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "9",
+      question:
+        "Do you often find difficulties in your social life such as making friends?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "10",
+      question:
+        "In the past six months, do you usually feel restless or tired?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "11",
+      question:
+        "Do you find yourself having difficulties in concentration, in the past six months?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "12",
+      question:
+        "In the six months time, have you avoided any social interactions?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "13",
+      question:
+        "Have you ever consumed alcohol or used drugs excessively in order to calm yourself down?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "14",
+      question:
+        "Has anyone from your family members ever had mental health problems?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "15",
+      question:
+        "Have you experienced any loneliness, mental break down, or any feeling of being down?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "16",
+      question:
+        "In the past 2 weeks did you lose interest in things you normally do?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "17",
+      question: "Have you wished to be dead in the past two months?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "18",
+      question:
+        "In the past six months, have you ever had yourself isolated from others?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "19",
+      question: "Do you have a supportive social network or family?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
+    {
+      id: "20",
+      question:
+        "Do you usually act first and then think, in the last six months?",
+      options: [`Yes`, `No`, `N/A`],
+      answer: `Yes`,
+    },
   ],
-  
-}
+};
 
-export default surveyData
+export default surveyData;
