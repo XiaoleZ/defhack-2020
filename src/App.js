6e0e5b12d7351bd58c5d5024c8971d9f8596dee0
XiaoleZ <xiaole2@ualberta.ca>
2020-06-20 20:41:05 -0700

    access journal through url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NarBar";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Survey from "./components/Survey";
-import JournalPost from "./components/JournalPost";
+import Journal from "./components/Journal";
 
 // import logo from './logo.svg';
 
@@ -12,28 +12,7 @@ import './App.css';
 
 
 class App extends Component {
-  //hardcode
-  state = {
-    post: [
-      {
-        id: 1,
-        author: "Xiaole",
-        createdAt: "3 min ago",
-        post: "old post",
-      },
-      {
-        id: 2,
-        author: "Xiaole",
-        createdAt: "1 hour ago",
-        post: "old old post",
-      },
-    ],
-  };
-  //
-
   render() {
-    //hardcode
-    const { post } = this.state;
     return (
       <Router>
         <div className="App">
@@ -42,11 +21,7 @@ class App extends Component {
           <Route exact path="/signin" component={SignIn} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/survey" component={Survey} />
-          <Route exact path="/journel">
-            {/* <div className="post-container">
-            {post && post.map(post => <JournalPost key={post.id} data={post} />)}
-          </div> */}            
-          </Route>
+          <Route exact path="/journal" component={Journal} />         
         </div>
         </div>
 
