b8f7e284390aa0b360799e4f58c121cadc58c0b8
Myat Thu Ko <nymyatthuko@gmail.com>
2020-06-20 22:59:02 -0400

    Fixed the error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import NavBar from './components/NarBar';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
-import Survey from './components/Survey';
-import JournalPost from './components/JournalPost';
+import React, { Component } from "react";
+import NavBar from "./components/NarBar";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import Survey from "./components/Survey";
+import JournalPost from "./components/JournalPost";
 
 // import logo from './logo.svg';
 import "./App.css";
@@ -11,16 +11,20 @@ import "./App.css";
 class App extends Component {
   //hardcode
   state = {
-    post: [{
-      id:1,
-      author: "Xiaole",
-      createdAt: "3 min ago",
-      post:"old post"},
+    post: [
       {
-        id:2,
+        id: 1,
+        author: "Xiaole",
+        createdAt: "3 min ago",
+        post: "old post",
+      },
+      {
+        id: 2,
         author: "Xiaole",
         createdAt: "1 hour ago",
-        post:"old old post"},]
+        post: "old old post",
+      },
+    ],
   };
   //
 
@@ -32,10 +36,12 @@ class App extends Component {
         <NavBar />
         {/* <SignIn /> */}
         {/* <SignUp /> */}
-        <Survey/>
+        <Survey />
         <div className="post-container">
-          {post && post.map(post => <JournalPost key={post.id} data={post} />)}
+          {post &&
+            post.map((post) => <JournalPost key={post.id} data={post} />)}
         </div>
+      </div>
     );
   }
 }
