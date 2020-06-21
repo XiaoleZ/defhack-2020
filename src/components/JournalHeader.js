be04e39bef670590b4a0bc9955ff771172a0e564
Mella Liang <mellaliang47@gmail.com>
2020-06-21 02:50:30 -1000

    updated journal header css

--- src/components/JournalHeader.js
+++ src/components/JournalHeader.js
@@ -4,8 +4,8 @@ import './Journal.css'
 const PostHeader = props => (
   <div className="post-header-container">
     <div className="data-container">
-      <p class="entryTime"> {props.data}</p>
-      <p class="entryName"><strong>{props.name}</strong></p>
+      <strong>{props.name}</strong>
+      <span class="entryTime"> {props.data}</span>
     </div>
   </div>
 );
